f047e75bd54683a73dd0b319f30f18dc3e760089
orensultan <orens2806@gmail.com>
2022-07-11 19:03:33 +0200

    parse data

--- dataset/read_data.py
+++ dataset/read_data.py
@@ -1,6 +1,10 @@
 
 import json
 
+import csv
+import pandas as pd
+
+output_file_name = 'echr_arguments.csv'
 
 def read_data():
     # Opening JSON file
@@ -21,14 +25,35 @@ def read_data():
         arguments = []
         for argument in record['arguments']:
             premises = [p for p in argument['premises']] if type(argument['premises']) == list else [argument['premises']]
-            conclusions = [c for c in argument['conclusion']] if type (argument['conclusion']) == list else [argument['conclusion']]
+            # conclusions = [c for c in argument['conclusion']] if type (argument['conclusion']) == list else [argument['conclusion']]
             argument_premises = [clauses_id_to_text[p] for p in premises]
-            argument_conclusions = [clauses_id_to_text[c] for c in conclusions]
-            arguments.append({'premises': argument_premises, 'conclusion': argument_conclusions})
+            # argument_conclusions = [clauses_id_to_text[c] for c in conclusions]
+            arguments += argument_premises
 
-        all_data_arguments.append(arguments)
-    print(1)
+        for arg in arguments:
+            all_data_arguments.append((text_name, arg))
+    return all_data_arguments
+
+
+
+
+def write_arguments_to_csv(arguments, output_file_name):
+    header = ['id', 'name', 'argument']
+
+    with open(output_file_name, 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        id = 1
+        for name, argument in arguments:
+            data = [id, name, argument]
+            writer.writerow(data)
+            id += 1
+    f.close()
+
+
+
 
 
 if __name__ == '__main__':
-    read_data()
+    arguments = read_data()
+    write_arguments_to_csv(arguments, output_file_name)
